21d369759f531a5aab198c993ac569e607ce1160
RVanEeden85 <104363539+RVanEeden85@users.noreply.github.com>
2025-11-03 20:21:37 -0500

    update Contact Me Page form input

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -14,11 +14,13 @@ export default function ContactMeBlock() {
     });
     const [status, setStatus] = useState("");
 
-    const handleChange = (e) => {
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setStatus("Sending...");
 
